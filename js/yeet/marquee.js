a291110a59584958bbfa640d59f9fe85b2619340
S4WA <s4wa@outlook.com>
2019-02-03 03:45:32 +0900

    commit

--- js/yeet/marquee.js
+++ js/yeet/marquee.js
@@ -25,8 +25,8 @@ $(document).ready(function() {
 
 	window.SetVolume = function(val) {
 		var player = document.getElementById('video');
-		console.log('Before: ' + player.volume);
+		// console.log('Before: ' + player.volume);
 		player.volume = val / 100;
-		console.log('After: ' + player.volume);
+		// console.log('After: ' + player.volume);
 	}
 });
