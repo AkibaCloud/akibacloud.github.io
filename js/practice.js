8608ee71b5384d77416da68b566e75536885edfb
S4WA <s4wa@outlook.com>
2019-05-19 02:22:43 +0900

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -14,34 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 });
 
-window.onload = () => {
-	registerAllWindows();
-}
-
-function registerAllWindows() {
-	var windows = $(".window");
-	for (var i = 0; i < windows.length; i++) {
-		var aFrame = new frame(), rect = windows[i].getBoundingClientRect();
-		aFrame.x = rect.left;
-		aFrame.y = rect.top;
-		aFrame.name = $(".bar")[i].innerText.clean();
-		aFrame.body = windows[i];
-		list.push(aFrame);
-	}
-	if (!list.isEmpty()) {
-		currentWindow = list[0];
-	}
-}
-
 function getFrameFromElement(element) {
 	for (var i = 0; i < list.length; i++) {
 		if (list[i].body == element) return list[i];
 	}
-}
-
-function frame() {
-	var x = 0, y = 0;
-	var name, body;
 }
 
 function update(elem) {
@@ -49,5 +25,3 @@ function update(elem) {
 	$(".window").removeClass("forward").addClass("backward");
 	$(elem).removeClass("backward").addClass("forward");
 }
-
-var list = new Array(), currentWindow;
