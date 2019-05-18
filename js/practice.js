b01d30687606869fb69bfdb0fecd74c222649f94
S4WA <s4wa@outlook.com>
2019-05-18 20:58:07 +0900

    Update files

--- js/practice.js
+++ js/practice.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-	
+	var windows = document.getElementsByClassName("window");
+	for (var i = 0; i < windows.length; i++) {
+		var aFrame = new frame();
+		aFrame.x = Number(windows[i].style.width);
+		aFrame.y = Number(windows[i].style.height);
+		list.push(aFrame);
+	}
 });
+
+function frame() {
+	var x = 0, y = 0;
+}
+
+const list = new Array();
