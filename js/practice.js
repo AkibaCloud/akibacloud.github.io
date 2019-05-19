6596ab37421ff0769a78097f8a02c11a0973fe2f
S4WA <s4wa@outlook.com>
2019-05-19 14:22:04 +0900

    Update files

--- js/practice.js
+++ js/practice.js
@@ -3,25 +3,52 @@
 */
 
 document.addEventListener("DOMContentLoaded", () => {
+	registerWindows();
+	registerTabs();
+	renderTime();
+});
+
+function registerWindows() {
 	$(".window").draggable({
 		handle: ".bar"
 	});
 	$(".window").on("drag", function() {
-		update($(this));
+		updateWindow($(this));
 	});
 	$(".window").on("click", function() {
-		update($(this));
+		updateWindow($(this));
 	});
-});
-
-function getFrameFromElement(element) {
-	for (var i = 0; i < list.length; i++) {
-		if (list[i].body == element) return list[i];
-	}
 }
 
-function update(elem) {
+function registerTabs() {
+	$(".tab ul.title li").addClass("other");
+	$(".tab ul.title li:first-child").removeClass("other").addClass("current");
+}
+
+function updateWindow(elem) {
 	$("#currentWindow").text(elem.find(".title").text());
 	$(".window").removeClass("forward").addClass("backward");
 	$(elem).removeClass("backward").addClass("forward");
+}
+
+function renderTime() {
+	var currentTime = new Date(), diem = "am";
+	var h = currentTime.getHours(), m = currentTime.getMinutes(), s = currentTime.getSeconds();
+	setTimeout("renderTime();", 1000);
+	if (h == 0) {
+		h = 12;
+	} else if (h > 12) {
+		h = h - 12;
+		diem = "pm";
+	}
+	if (h < 10) {
+		h = "0" + h;
+	}
+	if (m < 10) {
+		m = "0" + m;
+	}
+	if (s < 10) {
+		s = "0" + s;
+	}
+	$("#time :first-child").text(h + ":" + m + ":" + s + " " + diem);
 }
