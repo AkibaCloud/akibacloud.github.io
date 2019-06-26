6a072f9b0a1956949c46b97ca424a99c83c964e4
S4WA <s4wa@outlook.com>
2019-06-27 08:55:43 +0900

    Update files

--- js/190625.js
+++ js/190625.js
@@ -1,8 +1,3 @@
 $( function() {
 	$("#t1").css("top", ( $("#t1").text().length*6 ) + "px");
-
-	var tag = $("a");
-	for (var i = 0; i < tag.length; i++) {
-		$(tag[i]).attr("title", $(tag[i]).text());
-	}
 } );
