85e8e02b7a0fba23c24f26984d839067e716579f
S4WA <s4wa@outlook.com>
2019-06-26 10:22:05 +0900

    Update 190625.js

--- js/190625.js
+++ js/190625.js
@@ -1,5 +1,6 @@
 $( function() {
 	$("#t1").css("top", ( $("#t1").text().length*6 ) + "px");
+
 	var tag = $("a");
 	for (var i = 0; i < tag.length; i++) {
 		$(tag[i]).attr("title", $(tag[i]).text());
