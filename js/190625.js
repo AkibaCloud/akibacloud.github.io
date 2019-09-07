c4628ad8bd2dc80df8319640543ef420eee7a672
Drip <drip@outlook.jp>
2019-09-08 02:02:02 +0900

    Update files

--- js/190625.js
+++ js/190625.js
@@ -2,8 +2,12 @@ $( function() {
 	$("#toggle-font").on("click", () => {
 		var text = $(".right-text"), btn = $("#toggle-font");
 
+		for (var i = 0; i < text.length; i++) {
+			if ( $(text[i]).hasClass("ignore-font") ) text[i] = null;
+		}
+
 		if (text.css("font-family") == "UnifrakturCook") {
-			text.css("font-family", "MS UI Gothic");
+			text.css("font-family", "Meiryo");
 			btn.css("background-image", "linear-gradient(225deg, #d7d2cc 0%, #304352 100%)");
 		} else {
 			text.css("font-family", "UnifrakturCook");
